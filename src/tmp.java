0c1d93d9d55303b50ef7a66054608805edb56548
Barash Anton <BarashASUA@gmail.com>
2023-05-18 20:58:07 +0300

    tmp add psvm

--- src/tmp.java
+++ src/tmp.java
@@ -1,5 +1,46 @@
 public class tmp {
+
+    static String perevircaMethod(int a, int b, int c) {
+        int tmpUp = 0;
+        int tmpZero = 0;
+        int tmdDw = 0;
+
+
+        if (a > 0) {
+            tmpUp++;
+        } else if (a < 0) {
+            tmdDw++;
+        } else {
+            tmpZero++;
+        }
+
+        if (b > 0) {
+            tmpUp++;
+        } else if (b < 0) {
+            tmdDw++;
+
+        } else {
+            tmpZero++;
+        }
+
+        if (c > 0) {
+            tmpUp++;
+        } else if (c < 0) {
+            tmdDw++;
+        } else {
+            tmpZero++;
+        }
+
+
+        return " Більше нуля " + tmpUp + "\n менше нуля " + tmdDw + "\n рівно нулю  " + tmpZero;
+    }
+
+
     public static void main(String[] args) {
 
+//        int res = perevircaMethod(-45, 5, 9);
+//        System.out.println(res);
+
+        System.out.println(perevircaMethod(2, 7, -12));
     }
 }

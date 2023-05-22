139a12880c299931ff15e1f9fdc6c3f503324e09
Barash Anton <BarashASUA@gmail.com>
2023-05-23 01:41:27 +0300

    Merge remote-tracking branch 'CBS/master'

--- src/GitCourse/Leason2/Task2/Task2_NewBranch3.java
+++
@@ -1,6 +0,0 @@
-package GitCourse.Leason2.Task2;
-
-public class Task2_NewBranch3 {
-    // Це файл 3 в Вовій гілці NewBranch
-
-}
--- src/GitCourse/Leason2/Task2/Task2_NewBranch5.java
+++
@@ -1,5 +0,0 @@
-package GitCourse.Leason2.Task2;
-
-public class Task2_NewBranch5 {
-    // Це файл 5 в Вовій гілці NewBranch
-}

3b264414eea25b9783d4ca6442845a69ca8604aa
Barash Anton <BarashASUA@gmail.com>
2023-06-09 03:22:49 +0300

    OOP lesson5 converter temperature - done

---
+++ src/JavaEssential/Leason5/Task2/Zoo.java
@@ -0,0 +1,39 @@
+package JavaEssential.Leason5.Task2;
+
+import java.util.ArrayList;
+
+public class Zoo {
+    public static void main(String[] args) {
+        ArrayList<String> Zoo = new ArrayList<>();
+
+        System.out.println(Zoo.size());
+        System.out.println(Zoo);
+
+        Zoo.add(0,"Лев");
+        Zoo.add(1,"Тигр");
+        Zoo.add(2,"Пума");
+        Zoo.add(3,"Ягуар");
+        Zoo.add(4,"Жираф");
+        Zoo.add(5,"Зебра");
+        Zoo.add(6,"Слон");
+        Zoo.add(7,"Носоріг");
+        Zoo.add(8,"Капібара");
+
+        System.out.println(Zoo.size());
+        System.out.println(Zoo);
+
+        Zoo.remove(3);
+        Zoo.remove(5);
+        Zoo.remove(6);
+
+        System.out.println(Zoo.size());
+        System.out.println(Zoo);
+
+
+
+
+
+
+
+    }
+}

6591182c164368293e6f16315a3a01613ae6ea59
Barash Anton <BarashASUA@gmail.com>
2023-07-13 12:46:53 +0300

    HW 8 OOP

--- src/JavaAdvance/Leason1/Task3/ex_004_comparable/interface_comparable/Car.java
+++ src/JavaAdvance/Leason1/Task3/ex_004_comparable/interface_comparable/Car.java
@@ -1,6 +1,9 @@
 package JavaAdvance.Leason1.Task3.ex_004_comparable.interface_comparable;
 
-public class Car implements Comparable { //<Car>{
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Car { //<Car>{
     int speed;
     int price;
     String model;
@@ -17,15 +20,40 @@ public class Car implements Comparable { //<Car>{
         return this.model + " " + this.color + " " + this.speed + " " + this.price;
     }
 
+    //     Сортируем по скорости/цене
+//    @Override
+//    public int compareTo(Car o) {    // public int compareTo(Car o) {
+//        int temp = this.speed - ((Car) o).speed;   // int temp = this.speed - o.speed;
+//        if (temp == 0) {
+//            return this.price - ((Car) o).price;
+//        } else if (temp == 0)  {
+//            return this.model.compareTo(model);
+//        } else {
+//            return this.color.compareTo(color);
+//        }
+//    }
+
+
+//    @Override
+//    public int compareTo(Car o) {
+//        return this.speed - ((Car) o).speed;
+//        return this.price - ((Car) o).price;
+//        return this.model.compareTo(model);
+//        return this.color.compareTo(color);
+//        return 0;
+//    }
+
+
     // Сортируем по скорости/цене
-    public int compareTo(Object o) {    // public int compareTo(Car o) {
-        int temp = this.speed - ((Car)o).speed;   // int temp = this.speed - o.speed;
-        if (temp == 0) {
-            return this.price - ((Car)o).price;
-        } else {
-            return temp;
-        }
-    }
+//    public int compareTo(Car o) {    // public int compareTo(Car o) {
+//        int temp = this.speed - ((Car) o).speed;   // int temp = this.speed - o.speed;
+//        if (temp == 0) {
+//            return this.price - ((Car) o).price;
+//        } else {
+//            return temp;
+//        }
+//    }
+
 
     // Сравнение 2-х строковых значений
     // return this.model.compareTo((Car)o).model);
